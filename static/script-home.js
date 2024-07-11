58387c86a68f48a490c5f54f0bd0c4962869930d
settor21 <43499539+settor21@users.noreply.github.com>
2024-07-11 05:22:00 +0100

    WIP

--- static/script-home.js
+++ static/script-home.js
@@ -33,7 +33,7 @@ var pizzaTypes = [
     "The Prosciutto and Arugula Pizza",
     "The Big Meat Pizza",
     "The Spicy Italian Pizza"
-  ];
+];
 
 var pizzaOptions = pizzaTypes.map(function(pizzaType) {
     return `<option value="${pizzaType.toLowerCase().replace(/ /g, "-")}">${pizzaType}</option>`;
@@ -47,65 +47,66 @@ quantityElement = document.getElementById('quantity');
 costElement = document.getElementById('cost');
 console.log("costElement:", costElement);
 
-costElement.addEventListener('change', function() { 
-  updateTotalCost()
+costElement.addEventListener('change', function() {
+    updateTotalCost()
 });
-pizzaTypeElement.addEventListener('change', function() { 
-  updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement) 
+pizzaTypeElement.addEventListener('change', function() {
+    updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement)
 });
-pizzaSizeElement.addEventListener('change', function() { 
-  updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement) 
+pizzaSizeElement.addEventListener('change', function() {
+    updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement)
 });
-quantityElement.addEventListener('change', function() { 
-  updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement) 
+quantityElement.addEventListener('change', function() {
+    updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement)
 });
 
 
 function updateTotalCost() {
-  // console.log("Updating Total Cost");
-  let totalCost = 0;
-  for (let costElement of document.querySelectorAll('#cost')) {
-    console.log("Cost Element:", costElement);
-    console.log("TCost:", costElement.textContent);
-    totalCost += parseFloat(costElement.textContent);
-  }
-  console.log("Total Cost:", totalCost); 
-  console.log("TCOST ELEMENT", totalCostElement);
-  totalCostElement.textContent = totalCost;
+    // console.log("Updating Total Cost");
+    let totalCost = 0;
+    for (let costElement of document.querySelectorAll('#cost')) {
+        console.log("Cost Element:", costElement);
+        console.log("TCost:", costElement.textContent);
+        totalCost += parseFloat(costElement.textContent);
+    }
+    console.log("Total Cost:", totalCost);
+    console.log("TCOST ELEMENT", totalCostElement);
+    totalCostElement.textContent = totalCost;
 }
 
 function updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement) {
-  console.log("Pizza Type Element:", pizzaTypeElement, "Pizza Size Element:", pizzaSizeElement, "Quantity Element:", quantityElement, "Cost Element:", costElement);
-  let pizzaType = pizzaTypeElement.options[pizzaTypeElement.selectedIndex].text;
-  let pizzaSize = pizzaSizeElement.value;
-  let quantity = quantityElement.value;
+    console.log("Pizza Type Element:", pizzaTypeElement, "Pizza Size Element:", pizzaSizeElement, "Quantity Element:", quantityElement, "Cost Element:", costElement);
+    let pizzaType = pizzaTypeElement.options[pizzaTypeElement.selectedIndex].text;
+    let pizzaSize = pizzaSizeElement.value;
+    let quantity = quantityElement.value;
 
-  getPizzaCost(pizzaType, pizzaSize, quantity)
-  .then(totalCost => {
-    costElement.textContent = totalCost;
-    let event = new Event('change');
-    costElement.dispatchEvent(event);
-  });
+    getPizzaCost(pizzaType, pizzaSize, quantity)
+        .then(totalCost => {
+            costElement.textContent = totalCost;
+            let event = new Event('change');
+            costElement.dispatchEvent(event);
+        });
 }
 
 function getPizzaCost(pizzaType, pizzaSize, quantity) {
-  return fetch(`/pizza-cost/${pizzaType}/${pizzaSize}`)
-    .then(response => response.json())
-    .then(data => {
-      // data.cost is the cost of the pizza
-      console.log("Data Cost:", data.cost, "Quantity:", quantity);
-      let totalCost = data.cost * quantity;
-      console.log("Cost:", totalCost);
-      return totalCost;
-    });
+    return fetch(`/pizza-cost/${pizzaType}/${pizzaSize}`)
+        .then(response => response.json())
+        .then(data => {
+            // data.cost is the cost of the pizza
+            console.log("Data Cost:", data.cost, "Quantity:", quantity);
+            let totalCost = data.cost * quantity;
+            console.log("Cost:", totalCost);
+            return totalCost;
+        });
 }
 
 document.getElementById('add-pizza').addEventListener('click', function() {
     var pizzaOrderFields = document.getElementsByClassName('pizza-order-fields')[0];
-  
+
     var newPizzaOrderFields = document.createElement('div');
     newPizzaOrderFields.className = `pizza-group-${pizzaGroupCounter}`;
     newPizzaOrderFields.innerHTML = `
+      
       <div class="form-group">
         <label for="pizza_size">Size:</label>
         <select id="pizza_size" name="pizza_size">
@@ -135,30 +136,30 @@ document.getElementById('add-pizza').addEventListener('click', function() {
     let pizzaSizeElement = newPizzaOrderFields.querySelector('#pizza_size');
     let quantityElement = newPizzaOrderFields.querySelector('#quantity');
     let costElement = newPizzaOrderFields.querySelector('#cost');
-    
+
     console.log("costElement:", costElement);
-    costElement.addEventListener('change', function() { 
-      updateTotalCost()
+    costElement.addEventListener('change', function() {
+        updateTotalCost()
     });
-    pizzaTypeElement.addEventListener('change', function() { 
-      updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement) 
+    pizzaTypeElement.addEventListener('change', function() {
+        updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement)
     });
-    pizzaSizeElement.addEventListener('change', function() { 
-      updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement) 
+    pizzaSizeElement.addEventListener('change', function() {
+        updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement)
     });
-    quantityElement.addEventListener('change', function() { 
-      updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement) 
+    quantityElement.addEventListener('change', function() {
+        updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement)
     });
 
     updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement);
     newPizzaOrderFields.querySelector('.remove-pizza').addEventListener('click', function() {
         pizzaOrderFields.removeChild(newPizzaOrderFields);
         updateTotalCost();
-      });
+    });
     pizzaOrderFields.appendChild(newPizzaOrderFields);
 
     pizzaGroupCounter++
-  });
+});
 
 // Call updateCost immediately when the script loads
 updateCost(pizzaTypeElement, pizzaSizeElement, quantityElement, costElement);
